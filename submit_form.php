c60f56861afda0dddc827444d956fac7fa3af13d
Tejeswar Naidu .S <148424898+Tejeswar001@users.noreply.github.com>
2024-07-25 10:31:10 +0530

    Add files via upload

---
+++ submit_form.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "tejeswarnaidu9@gmail.com"; // Your email address
+    $headers = "From: $email";
+    $body = "Name: $name\nEmail: $email\nSubject: $subject\n\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
